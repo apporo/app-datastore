2642d96415ef295eb601d7daa3fbcaaab5be875e
pnhung177 <pnhung177@acegik.net>
2018-05-21 22:22:35 +0700

    Define wrap() private method

--- src/services/data-manipulator.js
+++ src/services/data-manipulator.js
@@ -12,11 +12,11 @@ function DataManipulator(params) {
 
   let self = this;
   let LX = params.loggingFactory.getLogger();
-  let LT = params.loggingFactory.getTracer();
+  let TR = params.loggingFactory.getTracer();
   let packageName = params.packageName || 'app-datastore';
   let blockRef = chores.getBlockRef(__filename, packageName);
 
-  LX.has('silly') && LX.log('silly', LT.toMessage({
+  LX.has('silly') && LX.log('silly', TR.toMessage({
     tags: [ blockRef, 'constructor-begin' ],
     text: ' + constructor start ...'
   }));
@@ -25,6 +25,45 @@ function DataManipulator(params) {
   let schemaManager = params['schemaManager'];
   let mongoAccessor = params['mongoose#manipulator'];
   let normalFieldsOf = {};
+
+  let getRequestId = function(opts) {
+    return (opts.requestId = opts.requestId || TR.getLogID());
+  }
+
+  let getRequestTracer = function(opts) {
+    return TR.branch({ key: 'requestId', value: getRequestId(opts) });
+  }
+
+  let beginTracing = function(tracer, methodName) {
+    LX.has('debug') && LX.log('debug', tracer.add({methodName}).toMessage({
+      tags: [ blockRef, methodName, 'begin' ],
+      text: ' - [${requestId}] ${methodName}() begin'
+    }));
+  }
+
+  let endTracing = function(tracer, methodName, args, opts, flow) {
+    return flow.then(function(result) {
+      LX.has('debug') && LX.log('debug', tracer.add({methodName}).toMessage({
+        tags: [ blockRef, methodName, 'completed' ],
+        text: ' - [${requestId}] ${methodName}() has completed'
+      }));
+      return result;
+    }).catch(function(error) {
+      LX.has('error') && LX.log('error', tracer.add({methodName, error}).toMessage({
+        tags: [ blockRef, methodName, 'failed' ],
+        text: ' - [${requestId}] ${methodName}() has failed. Error: ${error}'
+      }));
+      return Promise.reject(error);
+    });
+  }
+
+  let wrap = function(methodName, args, opts, main) {
+    args = args || {};
+    opts = opts || {};
+    let reqTr = getRequestTracer(opts);
+    beginTracing(reqTr, methodName);
+    return endTracing(reqTr, methodName, args, opts, Promise.resolve(main(reqTr, args, opts)));
+  }
 
   let getModel = function(name) {
     let model = schemaManager.getModel(name);
@@ -42,16 +81,17 @@ function DataManipulator(params) {
   };
 
   this.find = function(args, opts) {
-    args = args || {};
-    let {query={}, projection={}, options={}, from, size} = args;
-    options.skip = options.skip || from;
-    options.limit = options.limit || size;
-    let flow = getModel(args.type);
-    flow = flow.then(function(model) {
-      var p_find = Promise.promisify(model.find, {context: model});
-      return p_find(query, projection, options);
+    return wrap('find', args, opts, function(reqTr, args, opts) {
+      let {query={}, projection={}, options={}, from, size} = args;
+      options.skip = options.skip || from;
+      options.limit = options.limit || size;
+      let flow = getModel(args.type);
+      flow = flow.then(function(model) {
+        var p_find = Promise.promisify(model.find, {context: model});
+        return p_find(query, projection, options);
+      });
+      return flow;
     });
-    return flow;
   }
 
   this.get = function(args, opts) {
@@ -98,7 +138,7 @@ function DataManipulator(params) {
     return flow;
   }
 
-  LX.has('silly') && LX.log('silly', LT.toMessage({
+  LX.has('silly') && LX.log('silly', TR.toMessage({
     tags: [ blockRef, 'constructor-end' ],
     text: ' - constructor has finished'
   }));
