9a16c84bf348d2d5d2901837fd08094f828a8c15
emirp47 <tuanlt47@gmail.com>
2019-05-28 14:49:14 +0700

    Add populate and Count for data-manipulator

--- src/services/data-manipulator.js
+++ src/services/data-manipulator.js
@@ -17,7 +17,7 @@ function DataManipulator(params) {
   let blockRef = chores.getBlockRef(__filename, packageName);
 
   LX.has('silly') && LX.log('silly', TR.toMessage({
-    tags: [ blockRef, 'constructor-begin' ],
+    tags: [blockRef, 'constructor-begin'],
     text: ' + constructor start ...'
   }));
 
@@ -25,38 +25,38 @@ function DataManipulator(params) {
   let schemaManager = params['schemaManager'];
   let mongoAccessor = params['mongoose#manipulator'];
 
-  let getRequestId = function(opts) {
+  let getRequestId = function (opts) {
     return (opts.requestId = opts.requestId || TR.getLogID());
   }
 
-  let getRequestTracer = function(opts) {
+  let getRequestTracer = function (opts) {
     return TR.branch({ key: 'requestId', value: getRequestId(opts) });
   }
 
-  let beginTracing = function(tracer, methodName) {
-    LX.has('debug') && LX.log('debug', tracer.add({methodName}).toMessage({
-      tags: [ blockRef, methodName, 'begin' ],
+  let beginTracing = function (tracer, methodName) {
+    LX.has('debug') && LX.log('debug', tracer.add({ methodName }).toMessage({
+      tags: [blockRef, methodName, 'begin'],
       text: ' - [${requestId}] ${methodName}() begin'
     }));
   }
 
-  let endTracing = function(tracer, methodName, args, opts, flow) {
-    return flow.then(function(result) {
-      LX.has('debug') && LX.log('debug', tracer.add({methodName}).toMessage({
-        tags: [ blockRef, methodName, 'completed' ],
+  let endTracing = function (tracer, methodName, args, opts, flow) {
+    return flow.then(function (result) {
+      LX.has('debug') && LX.log('debug', tracer.add({ methodName }).toMessage({
+        tags: [blockRef, methodName, 'completed'],
         text: ' - [${requestId}] ${methodName}() has completed'
       }));
       return result;
-    }).catch(function(error) {
-      LX.has('error') && LX.log('error', tracer.add({methodName, error}).toMessage({
-        tags: [ blockRef, methodName, 'failed' ],
+    }).catch(function (error) {
+      LX.has('error') && LX.log('error', tracer.add({ methodName, error }).toMessage({
+        tags: [blockRef, methodName, 'failed'],
         text: ' - [${requestId}] ${methodName}() has failed. Error: ${error}'
       }));
       return Promise.reject(error);
     });
   }
 
-  let wrap = function(methodName, args, opts, main) {
+  let wrap = function (methodName, args, opts, main) {
     args = args || {};
     opts = opts || {};
     let reqTr = getRequestTracer(opts);
@@ -67,12 +67,12 @@ function DataManipulator(params) {
     }
     let flow = Promise.resolve().then(lodash.wrap(main(reqTr, args, opts)));
     if (transformer) {
-      flow = flow.then(function(result) {
+      flow = flow.then(function (result) {
         if (lodash.isFunction(transformer.transformOutput)) {
           return transformer.transformOutput(result, args, opts);
         }
         return result;
-      }).catch(function(error) {
+      }).catch(function (error) {
         if (transformer && lodash.isFunction(transformer.transformError)) {
           throw transformer.transformError(error, args, opts);
         }
@@ -82,7 +82,7 @@ function DataManipulator(params) {
     return endTracing(reqTr, methodName, args, opts, flow);
   }
 
-  let getModel = function(name) {
+  let getModel = function (name) {
     let model = schemaManager.getModel(name);
     if (!model) return Promise.reject(name);
     return Promise.resolve(model);
@@ -97,82 +97,105 @@ function DataManipulator(params) {
     return fields;
   };
 
-  this.find = function(args, opts) {
-    return wrap('find', args, opts, function(reqTr, args, opts) {
-      let {query={}, projection={}, options={}, from, size} = args;
+  this.find = function (args, opts) {
+    return wrap('find', args, opts, function (reqTr, args, opts) {
+      let { query = {}, projection = {}, options = {}, populates = [], from, size } = args;
       options.skip = options.skip || from;
       options.limit = options.limit || size;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
-        var p_find = Promise.promisify(model.find, {context: model});
-        return p_find(query, projection, options);
+      flow = flow.then(function (model) {
+        var doc = model.find(query, projection, options);
+        if (populates) {
+          populates.forEach(function (polulateArgs) {
+            doc.populate(polulateArgs);
+          });
+        }
+        return Promise.resolve(doc.exec());
       });
       return flow;
     });
   }
 
-  this.findOne = function(args, opts) {
-    return wrap('findOne', args, opts, function(reqTr, args, opts) {
-      let {query={}, projection={}, options={}} = args;
+  this.findOne = function (args, opts) {
+    return wrap('findOne', args, opts, function (reqTr, args, opts) {
+      let { query = {}, projection = {}, options = {} } = args;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
-        var p_findOne = Promise.promisify(model.findOne, {context: model});
-        return p_findOne(query, projection, options);
+      flow = flow.then(function (model) {
+        var doc = model.findOne(query, projection, options);
+        if (populates) {
+          doc.populate(populates);
+        }
+        return Promise.resolve(doc.exec());
       });
       return flow;
     });
   }
 
-  this.get = function(args, opts) {
-    return wrap('get', args, opts, function(reqTr, args, opts) {
+  this.get = function (args, opts) {
+    return wrap('get', args, opts, function (reqTr, args, opts) {
+      let { populates = [] } = args;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
-        var p_findById = Promise.promisify(model.findById, {context: model});
-        return p_findById(args.id);
+      flow = flow.then(function (model) {
+        var doc = model.findById(args.id);
+        if (populates) {
+          doc.populate(populates);
+        }
+        return Promise.resolve(doc.exec());
       });
       return flow;
     });
   }
 
-  this.create = function(args, opts) {
-    return wrap('create', args, opts, function(reqTr, args, opts) {
+  this.count = function(args, opts) {
+    args = args || {};
+    let {filter={}} = args;
+    let flow = getModel(args.type);
+    flow = flow.then(function(model) {
+      var p_count = Promise.promisify(model.count, {context: model});
+      return p_count(filter);
+    })
+    return flow;
+  }
+
+  this.create = function (args, opts) {
+    return wrap('create', args, opts, function (reqTr, args, opts) {
       let data = args.data;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
+      flow = flow.then(function (model) {
         var object = new model(lodash.pick(data, pickNormalFields(model)));
-        return Promise.promisify(object.save, {context: object})();
+        return Promise.promisify(object.save, { context: object })();
       });
       return flow;
     });
   }
 
-  this.update = function(args, opts) {
-    return wrap('update', args, opts, function(reqTr, args, opts) {
+  this.update = function (args, opts) {
+    return wrap('update', args, opts, function (reqTr, args, opts) {
       let _id = args.data && args.data._id || args._id || args.id;
       let data = args.data;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
-        let _update = Promise.promisify(model.update, {context: model});
-        return _update({_id: _id}, lodash.pick(data, pickNormalFields(model)));
+      flow = flow.then(function (model) {
+        let _update = Promise.promisify(model.update, { context: model });
+        return _update({ _id: _id }, lodash.pick(data, pickNormalFields(model)));
       });
       return flow;
     });
   }
 
-  this.delete = function(args, opts) {
-    return wrap('delete', args, opts, function(reqTr, args, opts) {
+  this.delete = function (args, opts) {
+    return wrap('delete', args, opts, function (reqTr, args, opts) {
       let _id = args.data && args.data._id || args._id || args.id;
       let flow = getModel(args.type);
-      flow = flow.then(function(model) {
-        let _delete = Promise.promisify(model.remove, {context: model});
-        return _delete({_id: _id});
+      flow = flow.then(function (model) {
+        let _delete = Promise.promisify(model.remove, { context: model });
+        return _delete({ _id: _id });
       });
       return flow;
     });
   }
 
   LX.has('silly') && LX.log('silly', TR.toMessage({
-    tags: [ blockRef, 'constructor-end' ],
+    tags: [blockRef, 'constructor-end'],
     text: ' - constructor has finished'
   }));
 };
